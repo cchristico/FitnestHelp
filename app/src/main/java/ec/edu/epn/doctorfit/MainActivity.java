34c30cb30ae53213aba40c08b0b440a594c34461
Pedro Cuasqui <Pedro Cuasqui>
2016-06-05 00:24:02 -0500

    cambio total de plantilla en todo el proyecto

--- app/src/main/java/ec/edu/epn/doctorfit/MainActivity.java
+++ app/src/main/java/ec/edu/epn/doctorfit/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+//sa
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -80,17 +80,14 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_inicio) {
+            // handle the inicio action
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_progreso) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_recetas_alimentos) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_recordatorios) {
 
         }
 
