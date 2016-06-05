f0513eec0844205d5aa24f74860b00b427e10509
Jonathan Morocho <jonita2005@gmail.com>
2016-06-05 00:34:05 -0500

    Se agrega clase con las entidades para generar las clases VO y dao, faltan las relaciones entre tablas

---
+++ app/src/main/java/ec/edu/epn/doctorfit/sqlite/DaoGeneradorEntidades.java
@@ -0,0 +1,81 @@
+package ec.edu.epn.doctorfit.sqlite;
+
+/**
+ * Created by jonathan on 5/6/16.
+ */
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Property;
+import de.greenrobot.daogenerator.Schema;
+/**
+ * Created by jonathan on 4/6/16.
+ */
+public class DaoGeneradorEntidades {
+
+    public static void main(String[] args) throws Exception {
+        //En este ejemplo hay un unico esquema de base de datos si hubiera mas deberian agregarse
+        Schema schema = new Schema(1, "ec.edu.epn.doctorfit.sqlite.db");
+
+        //Este metodo se encargara de crear la estructura de la tabla cliente
+        addTables(schema);
+
+        //Se encargara de generar el codigo correspondiente en la ruta que se le especifico
+        DaoGenerator daoGenerator = new DaoGenerator();
+        daoGenerator.generateAll(schema, "./app/src/main/java");
+    }
+    private static void addTables(Schema schema) {
+
+        // TABLA USUARIO
+        Entity usuario = schema.addEntity("Usuario");
+        usuario.addIdProperty().primaryKey().autoincrement();
+        usuario.addStringProperty("nombreUsuario").notNull();
+        usuario.addIntProperty("edad").notNull();
+        usuario.addStringProperty("sexo").notNull();
+        usuario.addFloatProperty("estatura").notNull();
+
+        // TABLA ESTADO
+        Entity estado = schema.addEntity("Estado");
+        estado.addIdProperty().primaryKey().autoincrement();
+        estado.addFloatProperty("peso").notNull();
+        estado.addFloatProperty("longitudAbdominal").notNull();
+        estado.addFloatProperty("longitudCadera").notNull();
+        estado.addDateProperty("fechaEstado").notNull();
+
+        // TABLA ESTADODESEADO
+        Entity estadoDeseado = schema.addEntity("EstadoDeseado");
+        estadoDeseado.addIdProperty().primaryKey().autoincrement();
+        estadoDeseado.addStringProperty("estadoFuturo").notNull();
+
+        // TABLA ALIMENTACIONSEDENTARISMO
+        Entity alimentacionSedentarismo = schema.addEntity("AlimentacionSedentarismo");
+        alimentacionSedentarismo.addIdProperty().primaryKey().autoincrement();
+        alimentacionSedentarismo.addFloatProperty("calorias").notNull();
+        alimentacionSedentarismo.addFloatProperty("grasas").notNull();
+        alimentacionSedentarismo.addFloatProperty("proteinas").notNull();
+        alimentacionSedentarismo.addFloatProperty("porcentajeSedentarismo").notNull();
+
+        // TABLA CONSEJO
+        Entity consejo = schema.addEntity("Consejo");
+        consejo.addIdProperty().primaryKey().autoincrement();
+        consejo.addStringProperty("textoInformativo").notNull();
+
+        //TABLA DIETA
+        Entity dieta = schema.addEntity("Dieta");
+        dieta.addIdProperty().primaryKey().autoincrement();
+        dieta.addStringProperty("tipoDieta").notNull();
+
+        // TABLA PLATILLO
+        Entity platillo = schema.addEntity("Platillo");
+        platillo.addIdProperty().primaryKey().autoincrement();
+        platillo.addStringProperty("nombrePlatillo").notNull();
+
+        // TABLA ALIMENTO
+        Entity alimento = schema.addEntity("Alimento");
+        alimento.addIdProperty().primaryKey().autoincrement();
+        alimento.addStringProperty("nombreAlimento").notNull();
+        alimento.addStringProperty("aporteNutricional").notNull();
+        alimento.addFloatProperty("porcentajeNutricional").notNull();
+        alimento.addStringProperty("tipoAlimento").notNull();
+
+    }
+}
